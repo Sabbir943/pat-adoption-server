11ca0d3d0abcd56851d4431e8144424ee55cf3da
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-05-21 22:28:25 +0600

    created api for adpot request

--- index.js
+++ index.js
@@ -1,119 +1,126 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-const express=require('express');
-const cors=require('cors');
-const dotenv=require('dotenv');
-const app=express();
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-const PORT=process.env.SERVER_PORT;
-const uri =process.env.MONGODB_URI;
+const PORT = process.env.SERVER_PORT;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
- serverApi: {
-version: ServerApiVersion.v1,
- strict: true,
- deprecationErrors: true,
- }
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
 });
 
 
 async function run() {
-try {
+   try {
 
- // await client.connect();
- const db=client.db('pet-adoption');
- const patCollection=db.collection('pet-collection');
- const myRequest=db.collection("myRequest");
+      // await client.connect();
+      const db = client.db('pet-adoption');
+      const patCollection = db.collection('pet-collection');
+      const myRequest = db.collection("myRequest");
 
-
-app.post('/addPets',async(req,res)=>{
- const petData=req.body;
- const result= await patCollection.insertOne(petData);
- res.json(result);
- })
-
-app.get('/addPets',async(req,res)=>{
- const result=await patCollection.find().toArray();
- res.json(result);
- })
-
- // for role based users:--->
- app.get('/addPets/:email',async(req,res)=>{
-   const email=req.params.email;
-   const result = await patCollection.find({
-      ownerEmail:email
-   }).toArray();
-   res.json(result)
- })
- // created api for deleted
-
- app.delete('/addPets/:id',async(req,res)=>{
- const {id}=req.params
- const result = await patCollection.deleteOne({
- _id:new ObjectId(id)
- })
-res.json(result);
- })
-
- // created  api for updated in single card
- app.patch('/addPets/:id',async(req,res)=>{
- const{id}=req.params;
- const updateData=req.body;
- const result =await patCollection.updateOne(
- {_id: new ObjectId(id)},
-{$set:updateData}
-
- )
- res.json(result);
- })
-
- //for my request pet 
- app.post('/myRequest',async(req,res)=>{
- const petData=req.body;
- const result=await myRequest.insertOne(petData);
- res.json(result);
- })
-
- app.get('/myRequest',async(req,res)=>{
-   const reqData=req.body;
-   const result=await myRequest.find().toArray();
-   res.json(result);
- })
-
-app.get('/bookings/:userId', async (req, res) => {
- const { userId } = req.params; 
- const result = await bookingCollection.find({ userEmail: userId }).toArray();
- res.json(result);
-})
+      const requestCollection = db.collection("requests");
 
 
 
- app.get('/addPets/:id',async(req,res)=>{
-const {id}=req.params;
-const result= await patCollection.findOne({
- _id:new ObjectId(id)
-})
-res.json(result);
- })
+      app.post('/addPets', async (req, res) => {
+         const petData = {
+            ...req.body, status: 'available'
+         }
+         const result = await patCollection.insertOne(petData);
+         res.json(result);
+      })
 
- // await client.db("admin").command({ ping: 1 });
- console.log("Pinged your deployment. You successfully connected to MongoDB!");
- } finally {
 
- // await client.close();
-}
+      app.get('/addPets', async (req, res) => {
+         const result = await patCollection.find().toArray();
+         res.json(result);
+      })
+
+      // for role based users:--->
+      app.get('/myListing/:email', async (req, res) => {
+         const email = req.params.email;
+         const result = await patCollection.find({
+            ownerEmail: email
+         }).toArray();
+         res.json(result)
+      })
+      // for signle pet
+      app.get('/pet/:id', async (req, res) => {
+         const { id } = req.params
+         const result = await patCollection.findOne({
+            _id: new ObjectId(id)
+         })
+         res.json(result);
+      })
+      // updated the pet
+      app.patch('/pet/:id', async (req, res) => {
+         const { id } = req.params;
+         const updateData = req.body;
+         const result = await patCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: updateData }
+
+         )
+         res.json(result);
+      })
+
+      //Delete pet
+      app.delete('/pet/:id', async (req, res) => {
+         const { id } = req.params
+         const result = await patCollection.deleteOne({
+            _id: new ObjectId(id)
+         })
+         res.json(result);
+      })
+
+      //adopt request
+      app.post('/request', async (req, res) => {
+         const body = req.body;
+         const pet = await patCollection.findOne({ _id: new ObjectId(body.petId) })
+
+         if (pet.ownerEmail === body.userEmail) {
+            return res.status(400).json({
+               message: 'Owner cannot adopt'
+            })
+         }
+         if (pet.status === 'adopted') {
+            return res.status(400).json({
+               message: 'Already adopted'
+            })
+         }
+         const result = await requestCollection.insertOne({
+            ...body, status: "pending",
+            requestDate: new Date()
+         })
+         res.json(result);
+      })
+
+      
+
+      // await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+
+      // await client.close();
+   }
 }
 run().catch(console.dir);
 
 
 
-app.get('/',async(req,res)=>{
- res.send('server is ready!!!!!');
+app.get('/', async (req, res) => {
+   res.send('server is ready!!!!!');
 })
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
    console.log(`server is running is port ${PORT}`);
 })
