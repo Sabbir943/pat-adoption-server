0f9fdf97da7efe85fa3ebf3306fd746589c0ded8
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-05-24 11:47:14 +0600

    aded

--- index.js
+++ index.js
@@ -20,36 +20,36 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-const JWKS = createRemoteJWKSet(
-  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
-)
-const verifyToken = async (req, res, next) => {
-  const authHeader = req?.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({
-      message: "Unauthorized"
-    })
-  }
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({
-      message: "Unauthorized"
-    })
-  }
-  try {
-    const { payload } = await jwtVerify(token, JWKS);
-    req.user = payload;
-    next();
-  }
-  catch (e) {
-    return res.status(401).json({
-      message: "unauthorized"
-    })
-  }
+// const JWKS = createRemoteJWKSet(
+//   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+// )
+// const verifyToken = async (req, res, next) => {
+//   const authHeader = req?.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).json({
+//       message: "Unauthorized"
+//     })
+//   }
+//   const token = authHeader.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).json({
+//       message: "Unauthorized"
+//     })
+//   }
+//   try {
+//     const { payload } = await jwtVerify(token, JWKS);
+//     req.user = payload;
+//     next();
+//   }
+//   catch (e) {
+//     return res.status(401).json({
+//       message: "unauthorized"
+//     })
+//   }
 
 
 
-}
+// }
 
 async function run() {
   try {
@@ -62,7 +62,7 @@ async function run() {
  
 
     // ADD PET
-    app.post('/addPets',verifyToken ,async (req, res) => {
+    app.post('/addPets' ,async (req, res) => {
       const petData = {
         ...req.body,
         status: 'available'
@@ -76,7 +76,7 @@ async function run() {
 
 
     // GET SINGLE USER LISTINGS
-    app.get('/myListing/:email',verifyToken,async (req, res) => {
+    app.get('/myListing/:email',async (req, res) => {
       const result = await patCollection.find({
         ownerEmail: req.params.email
       }).toArray();
@@ -85,7 +85,7 @@ async function run() {
     });
 
     // GET SINGLE PET
-    app.get('/pet/:id', verifyToken, async (req, res) => {
+    app.get('/pet/:id',  async (req, res) => {
       const result = await patCollection.findOne({
         _id: new ObjectId(req.params.id)
       });
@@ -115,7 +115,7 @@ async function run() {
     /* -------------------- ADOPTION REQUEST -------------------- */
 
     // CREATE REQUEST
-    app.post('/request',verifyToken ,async (req, res) => {
+    app.post('/request',async (req, res) => {
       const body = req.body;
 
       const pet = await patCollection.findOne({
@@ -150,7 +150,7 @@ async function run() {
     });
 
     // GET MY REQUESTS (USER)
-    app.get('/myRequest/:email',verifyToken,async (req, res) => {
+    app.get('/myRequest/:email',async (req, res) => {
       const result = await requestCollection.find({
         userEmail: req.params.email
       }).toArray();
